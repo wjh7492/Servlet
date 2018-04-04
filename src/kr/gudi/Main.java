d69de526e4b571f9c67defc5d6ecd128ffc515a8
goodee <goodee@DESKTOP-V18QQLH>
2018-04-04 12:16:17 +0900

    파라메터 테스트 완료! (GET & POST)

--- src/kr/gudi/Main.java
+++ src/kr/gudi/Main.java
@@ -1,6 +1,7 @@
 package kr.gudi;
 
 import java.io.IOException;
+import java.util.Enumeration;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -12,11 +13,27 @@ public class Main extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		System.out.println("doGet");
+		getParam(req);
+		/*
+		String key = req.getParameter("key");
+		System.out.println(key);
+		*/
 	}
 
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		System.out.println("doPost");
+		req.setCharacterEncoding("UTF-8");
+		getParam(req);
+	}
+	
+	void getParam(HttpServletRequest req) {
+		Enumeration<String> names = req.getParameterNames();
+		while (names.hasMoreElements()) {
+			String paramName = (String) names.nextElement();
+			String value = req.getParameter(paramName);
+			System.out.println(paramName+ " : " + value);
+		}
 	}
 	
 }
